3b30930d01959bd84c3e50f5ba6daf3be8abb3ff
Kevin Boese <kmboese@ucdavis.edu>
2018-06-03 13:43:56 -0700

    Added merge and split functions (still debugging)

--- mergesort/c++/mergesort.cpp
+++ mergesort/c++/mergesort.cpp
@@ -3,12 +3,17 @@
 #include "mergesort.h"
 
 const bool DEBUG = true;
+std::stringstream debug_msg;
 
 //Print message if in DEBUG mode
-void dPrint(std::string message) {
-	if(DEBUG) {
-		std::cout << "DEBUG: " << message << std::endl;
+void dPrint(std::stringstream& ss) {
+	std::string s;
+	std::cout << "DEBUG: ";
+	while(std::getline(ss, s)) {
+		std::cout << s;
 	}
+	std::cout << "\n";
+	ss.clear();
 }
 
 //Print vector 
@@ -29,7 +34,7 @@ std::string toString(std::vector<int> &v) {
 }
 
 //Returns true if vector is sorted, false otherwise
-bool isSorted(std::vector<int> v) {
+bool isSorted(std::vector<int> &v) {
 	bool sorted = true;
 	for (auto it = v.begin(); it < v.end()-1; it++) {
 		if(*it > *(it+1) ) {
@@ -43,12 +48,13 @@ bool isSorted(std::vector<int> v) {
 //Bubble-sort algorithm for a vector
 void bubbleSort(std::vector<int> &v) {
     int tmp = 0;
-    for(auto i = v.begin(); i < v.end(); i++) {
-        for(auto j = i+1; j < v.end()-1; j++) {
-            if (*i > *j){
+    for(auto i = v.begin(); i < v.end()-1; i++) {
+        for(auto j = i+1; j < v.end(); j++) {
+            if (*i > *j) {
                 tmp = *i;
                 *i = *j;
                 *j = tmp;
+				//debug_msg << "i == " << *i << ", j == " << *j; dPrint(debug_msg);
             }
         }
     }
@@ -58,7 +64,28 @@ std::vector<int> merge(std::vector<int> &left, std::vector<int> &right) {
 	std::vector<int> ret{};
 	std::string sLeft = toString(left);
 	std::string sRight = toString(right);
-	dPrint("Left: " + sLeft + "\tRight " + sRight);
+	debug_msg << "Left: " << toString(left) << "\tRight: " << toString(right); dPrint(debug_msg);
+	return ret;
+}
+
+std::vector<std::vector<int> > split(std::vector<int> &v) {
+	std::vector< std::vector<int> > ret{};
+	std::vector<int> left{100}, right{100};
+	std::vector<int>::iterator it;
+
+	debug_msg << "v.size() == " << v.size() << ", v.size()/2 == " << v.size()/2; dPrint(debug_msg);
+
+	//Return the vector if it has less than two elements
+	if (v.size() < 2) {
+		ret.push_back(v);
+		return ret;
+	}
+	//Copy first half of vector to left vector
+	std::copy(v.begin(), v.begin()+3, left.begin());
+	ret.push_back(left);
+	//Copy the second half of the vector to the right vector
+	std::copy(v.begin()+3, v.end(), right.begin());
+	ret.push_back(right);
 
 	return ret;
 }
--- mergesort/c++/mergesort.h
+++ mergesort/c++/mergesort.h
@@ -2,12 +2,13 @@
 #define _MERGESORT_H
 #include <vector>
 #include <string>
+#include <sstream>
 
 //Returns true if a vector is sorted, false otherwise
-bool isSorted(std::vector<int> v);
+bool isSorted(std::vector<int> &v);
 
 //Prints a debug message if DEBUG flag is set to true
-void dPrint(std::string message);
+void dPrint(std::stringstream &ss);
 
 //print a vector
 void printVec(std::vector<int> &v);
@@ -18,6 +19,10 @@ std::string toString(std::vector<int> &v);
 //Sorts a vector using the bubble-sort algorithm
 void bubbleSort(std::vector<int> &v);
 
+//Merges two vectors into one sorted vector
+std::vector<int> merge(std::vector<int> &left, std::vector<int> &right);
 
+//Splits one vector into two halves
+std::vector<std::vector<int> > split(std::vector<int> &v);
 
 #endif 
