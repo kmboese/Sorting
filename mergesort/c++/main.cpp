c2aa621c16ddac18e98f80d3b101c47176391312
Kevin Boese <kmboese@ucdavis.edu>
2018-06-03 13:43:26 -0700

    added testing functions to main

--- mergesort/c++/main.cpp
+++ mergesort/c++/main.cpp
@@ -13,26 +13,61 @@ using namespace std;
 //Globally define random seed
 unsigned seed = chrono::system_clock::now().time_since_epoch().count();
 mt19937 gen(seed);
-uniform_int_distribution<int>  dist(0, UPPER_BOUND);
 
+
+//Format variables
+string divider = "_____________________________________________\n";
+
+//Testing Functions
+/*
+ * Generate an integer vector
+ * Arguments:
+ * 	size: Size of the vector to be generated
+ * 	lower_bound: lower bound of integers to be generated
+ * 	upper_bound: upper bound of integeres to be generated
+
+*/
+
+vector<int> genVector(int size, int lower_bound, int upper_bound) {
+	vector<int> ret{};
+	uniform_int_distribution<int> dist(lower_bound, upper_bound);
+	for (int i = 0; i < size; i++) {
+		ret.push_back(dist(gen));
+	}
+	return ret;
+}
+
+void testBubbleSort(vector<int>& v) {
+	//Save a copy of the original vector for comparison
+	vector<int> orig = v;
+	cout << "***** Testing Bubblesort ***** " << endl;
+	cout << "Unsorted vector: \n";
+	printVec(v);
+	cout << "Sorting vector with bubblesort...\n";
+	bubbleSort(v);
+	cout << "Sorted vector: \n";
+	//Assert that the size of the sorted vector is the same as the original vector
+	assert(v.size() == orig.size());
+	//Assert that the vector is indeed sorted
+	assert(isSorted(v));
+	printVec(v);
+
+}
 
 int main(int argc, char** argv){
 	vector<int> v = {5,6,3,4,1};
+	vector<int> left{}, right{};
 	vector<int> sorted = {1,2,3};
 
-	//Assertions
-	cout << "Asserting sorted vector is sorted..." << endl;
-	assert(isSorted(sorted));
+	//Function tests
+	testBubbleSort(v);
 
-	cout << "Original vector: ";
-	printVec(v);
+	vector<vector<int> > pair = split(v);
+	cout << "Printing left half...\n";
+	printVec(pair[0]);
+	cout << "Printing right half...\n";
+	printVec(pair[1]);
 
-	if(isSorted(v)){
-		cout << "Vector v is sorted" << endl;
-	}
-	else{
-		cout << "Vector v is not sorted " << endl;
-	}
 	return 0;
 }
 
